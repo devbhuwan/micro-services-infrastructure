1ff21830387362f9adefd3ef1656b771c4af5a1c
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-04-05 22:05:46 +0400

    initial draft

--- uaa-service/src/main/java/com/developerbhuwan/uaaservice/UaaServiceApplication.java
+++ uaa-service/src/main/java/com/developerbhuwan/uaaservice/UaaServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UaaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UaaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UaaServiceApplication.class, args);
+    }
 }
